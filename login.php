c125a4ee9cf93d9d0ac3ed33b2d364b2782afe2e
FranciscMoldovan <franciscmoldovan93@gmail.com>
2017-01-09 04:02:09 +0200

    Modified some stuff to fit my setup.

--- login.php
+++ login.php
@@ -11,14 +11,14 @@ else
 $username=$_POST['username'];
 $password=$_POST['password'];
 // Establishing Connection with Server by passing server_name, user_id and password as a parameter
-$connection = mysqli_connect("localhost", "root", "");
+$connection = mysqli_connect("localhost", "root", "root");
 // To protect MySQL injection for Security purpose
 $username = stripslashes($username);
 $password = stripslashes($password);
 $username = mysqli_real_escape_string($connection, $username);
 $password = mysqli_real_escape_string($connection, $password);
 // Selecting Database
-$db = mysqli_select_db($connection, "company");
+$db = mysqli_select_db($connection, "psncs_login");
 // SQL query to fetch information of registerd users and finds user match.
 $query = mysqli_query($connection, "select * from login where password='$password' AND username='$username'");
 
--- session.php
+++ session.php
@@ -1,8 +1,8 @@
 <?php
 // Establishing Connection with Server by passing server_name, user_id and password as a parameter
-$connection = mysqli_connect("localhost", "root", "");
+$connection = mysqli_connect("localhost", "root", "root");
 // Selecting Database
-$db = mysqli_select_db($connection, "company");
+$db = mysqli_select_db($connection, "psncs_login");
 session_start();// Starting Session
 // Storing Session
 $user_check=$_SESSION['login_user'];
